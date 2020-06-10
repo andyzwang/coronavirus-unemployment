02164c7dcd8ea0ead9e0b372e6cc02f64f2b7f12
Andy Wang <azwang@college.harvard.edu>
2020-06-10 11:28:02 -0500

    Add Colorado
    Added Colorado

--- states/CO/compile_CO.R
+++ states/CO/compile_CO.R
@@ -10,53 +10,51 @@ library(readxl)
 # processing
 # NOTE: range is manually set
 
-raw <- read_excel("AZ_data.xlsx", range = "A4:S21")
-
-head(raw)
+raw <- read_excel("CO_data.xlsx", range = "A2:BO14", na = "***")
 
 # import FIPS codes
 
 data(county.fips)
 
 output <- raw %>%
-
-  # remove unnecessary columns
-
-  select(-"Not Coded", -"Arizona", -"Unknown") %>%
-
+  
   # pivot long
-
+  
   pivot_longer(
-    cols = ends_with("County"), names_to = "county",
+    cols = Adams:Yuma, names_to = "county",
     values_to = "claims"
   ) %>%
+  
+  #remove total
+  
+  select(-'Colorado Total') %>%
+  clean_names(case = "snake") %>%
+  
   mutate(
-
+    
     # set general info
-    state = "Arizona",
-    state_fips = 4,
-    state_short = "AZ",
-    county = str_remove(county, " County"),
-
+    state = "Colorado",
+    state_fips = 8,
+    state_short = "CO",
+    
     # modify dates
-    date = ymd(Date),
+    date = week_ending_date,
     week = week(date),
     month = month(date),
     year = year(date),
-
+    
     # County FIPS code and name
-
-    polyname = case_when(
-      TRUE ~ paste("arizona,", tolower(county), sep = "")
-    )
+    
+    polyname = paste("colorado,", tolower(county), sep = "")
   ) %>%
+
   # Join with FIPS
 
   left_join(county.fips, by = "polyname") %>%
   rename(county_fips = fips) %>%
-
+  
   # relevant columns
-
+  
   select(
     state, state_fips, state_short, county, county_fips,
     date, week, month, year, claims
@@ -64,4 +62,4 @@ output <- raw %>%
   arrange(week)
 
 # output
-write.csv(output, file = "AZ_compiled.csv", row.names = FALSE)
+write.csv(output, file = "CO_compiled.csv", row.names = FALSE)
