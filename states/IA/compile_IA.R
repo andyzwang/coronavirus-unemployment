7969e3a1eada8af71fe84453f3471f3ad97c809a
Andy Wang <azwang@college.harvard.edu>
2020-06-10 14:01:13 -0500

    compile IA

---
+++ states/IA/compile_IA.R
@@ -0,0 +1,43 @@
+# imports
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(maps)
+library(stringr)
+
+# processing
+
+raw <- read_csv("IA_data.csv") %>%
+  clean_names(case = "snake")
+
+
+output <- raw %>%
+  select(fips, county, week_ending, initial_claims) %>%
+  rename(
+    county_fips = fips,
+    date = week_ending,
+    claims = initial_claims
+  ) %>%
+  mutate(
+
+    # dates
+    date = mdy(date),
+    week = week(date),
+    month = month(date),
+    year = year(date),
+
+    # set general info
+    state = "Iowa",
+    state_fips = 19,
+    state_short = "IA"
+  ) %>%
+  select(
+    state, state_fips, state_short, county, county_fips,
+    date, week, month, year, claims
+  ) %>%
+  filter(year >= 2020) %>%
+  arrange(week)
+
+# output
+write.csv(output, file = "IA_compiled.csv", row.names = FALSE)
