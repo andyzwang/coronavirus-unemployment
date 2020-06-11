b6f981020a212797a4be9fd448ac89dbe9a5cc5c
Andy Wang <azwang@college.harvard.edu>
2020-06-11 16:05:15 -0500

    change gather format

--- states/GA/compile_GA.R
+++ states/GA/compile_GA.R
@@ -5,25 +5,52 @@ library(janitor)
 library(lubridate)
 library(maps)
 library(stringr)
-library(tabulizer)
 
+# read in
 
 raw <- read_csv("GA_gathered.csv")
 
+# import FIPS codes
+
+data(county.fips)
+
 output <- raw %>%
-  rename(state = state_name,
-         state_short = state_abbrev,
-         county = county_name,
-         claims = initial_claims) %>%
+  pivot_longer(cols = !starts_with("county"), names_to = "month", values_to = "claims") %>%
   mutate(
-    date = ceiling_date(ymd(paste(year, month, "01", sep = "-")), "month") -1,
-    week = NA
+
+    # basic_info
+    state = "Georgia",
+    state_fips = 13,
+    state_short = "GA",
+
+    # dates
+    date = ceiling_date(ymd(
+      paste(2020, month, "01", sep = "-")
+    ), "month") - 1,
+    month = month(date),
+    week = week(date),
+    year = year(date),
+    # create polyname
+
+    polyname = case_when(
+      county == "DeKalb" ~ "georgia,de kalb",
+      TRUE ~ paste("georgia,", tolower(county), sep = "")
+    )
   ) %>%
+
+  # Join with FIPS codes
+
+  left_join(county.fips, by = "polyname") %>%
+  rename(county_fips = fips) %>%
+
+  # relevant columns
+
   select(
     state, state_fips, state_short, county, county_fips,
     date, week, month, year, claims
   ) %>%
-  arrange(month)
+  arrange(week)
+
 
 # output
 write.csv(output, file = "GA_compiled.csv", row.names = FALSE)
