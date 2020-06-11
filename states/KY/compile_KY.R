d7891dcca8c2901c1e4cc1973412deb6b287a38a
Andy Wang <azwang@college.harvard.edu>
2020-06-11 16:30:55 -0500

    change to have wide source

--- states/KY/compile_KY.R
+++ states/KY/compile_KY.R
@@ -10,21 +10,40 @@ library(stringr)
 
 raw <- read_csv("KY_gathered.csv")
 
+# import FIPS codes
+
 output <- raw %>%
-  rename(state = state_name,
-         state_short = state_abbrev,
-         county = county_name,
-         claims = initial_claims) %>%
+  pivot_longer(cols = !starts_with("county"), names_to = "date", values_to = "claims") %>%
   mutate(
-    date = ceiling_date(ymd(paste(year, month, "01", sep = "-")), "month") -1,
-    week = NA
+    
+    # date info
+    
+    date = ceiling_date(dmy(paste("1-", date)), "month") -1,
+    week = week(date),
+    month = month(date),
+    year = year(date),
+    
+    # set general info
+    state = "Kentucky",
+    state_fips = 22,
+    state_short = "KY",
+    
+    polyname = case_when(
+      TRUE ~ paste("kentucky,", tolower(county), sep = "")
+    )
+  ) %>%
+  
+  # Join with FIPS
+  
+  left_join(county.fips, by = "polyname") %>%
+  rename(
+    county_fips = fips
   ) %>%
   select(
     state, state_fips, state_short, county, county_fips,
     date, week, month, year, claims
   ) %>%
-  filter(year == 2020) %>%
-  arrange(month)
+  arrange(week)
 
 # output
 write.csv(output, file = "KY_compiled.csv", row.names = FALSE)
