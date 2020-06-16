a1fd0493694892daa89d04657be5bbdbe934470a
Andy Wang <azwang@college.harvard.edu>
2020-06-16 09:52:46 -0500

    format FIPS

--- initial_claims/UI_initial_compile.R
+++ initial_claims/UI_initial_compile.R
@@ -11,9 +11,17 @@ filenames <- list.files(".", recursive = T, pattern = "_compiled.csv")
 
 output <- Reduce(rbind, lapply(filenames, read.csv)) %>%
   filter(!is.na(claims)) %>%
-  filter(claims != 0)
+  filter(claims != 0) %>%
+  mutate(
+    county_fips = as.character(county_fips),
+    county_fips = case_when(
+      str_count(county_fips, "\\d") == 4~ paste("0", county_fips, sep = ""),
+      TRUE ~ county_fips
+    )
+  )
 
 # save
+
 
 write.csv(output, file = "UI_initial.csv", row.names = F)
 
