2998491ef701d3836e3d27d84f46b6d0e516b153
Andy Wang <azwang@college.harvard.edu>
2020-06-18 14:07:36 -0500

    update for wide input

--- initial_claims/MD/compile_MD.R
+++ initial_claims/MD/compile_MD.R
@@ -5,24 +5,44 @@ library(janitor)
 library(lubridate)
 library(maps)
 library(stringr)
-library(tabulizer)
 
+# import
+raw <- read_csv("MD_gathered.csv")
 
-raw <- read_csv("MD_gathered.csv") %>%
-  remove_empty("cols")
+# import FIPS codes
+
+data(county.fips)
 
 output <- raw %>%
-  rename(
-    state = state_name,
-    state_short = state_abbrev,
-    county = county_name,
-    claims = initial_claims
-  ) %>%
+  pivot_longer(cols = !starts_with("county"), names_to = "date", values_to = "claims") %>%
   mutate(
+    
+    # date info
+    
     date = mdy(date),
-    year = year(date),
     week = week(date),
-    month = month(date)
+    month = month(date),
+    year = year(date),
+    
+    # set general info
+    state = "Maryland",
+    state_fips = 24,
+    state_short = "MD",
+    
+    polyname = case_when(
+      county == "Baltimore County" ~ "maryland,baltimore",
+      county == "Prince George's" ~ "maryland,prince georges",
+      county == "Queen Anne's" ~ "maryland,queen annes",
+      county == "St. Mary's" ~ "maryland,st marys",
+      TRUE ~ paste("maryland,", tolower(county), sep = "")
+    )
+  ) %>%
+  
+  # Join with FIPS
+  
+  left_join(county.fips, by = "polyname") %>%
+  rename(
+    county_fips = fips
   ) %>%
   select(
     state, state_fips, state_short, county, county_fips,
