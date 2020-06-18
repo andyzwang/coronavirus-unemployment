3d0d90402b87ff3bd8ff1d0e3cebd1bd0030fa93
Andy Wang <azwang@college.harvard.edu>
2020-06-18 14:57:37 -0500

    update through 6/6

--- initial_claims/RI/compile_RI.R
+++ initial_claims/RI/compile_RI.R
@@ -13,8 +13,9 @@ library(readxl)
 raw <- read_excel("RI_data.xlsx", skip = 2) %>%
   filter(...1 == "UI Weekly Report Initial Claims")
 
-output <- raw %>%
+regions <- tibble(state_short = "RI", county = c("Bristol", "Kent","Newport","Providence","Washington"))
 
+output <- raw %>%
 
   pivot_longer(
     cols = starts_with("Week"), names_to = "date",
@@ -29,16 +30,29 @@ output <- raw %>%
     state = "Rhode Island",
     state_fips = 44,
     state_short = "RI",
-    county = NA,
     date = str_remove(date, "Week Ending "),
-    county_fips = NA,
 
     # modify dates
     date = mdy(paste(date, "2020", sep = " ")),
     week = week(date),
     month = month(date),
-    year = year(date)
+    year = year(date),
+    
+    # weird bc RI
+    claims = claims / 5
+    
   ) %>%
+  
+  left_join(regions, by = "state_short") %>%
+  mutate(
+    polyname = case_when(
+      TRUE ~ paste("rhode island,", tolower(county), sep = "")
+    )
+  ) %>%
+  # Join with FIPS
+  
+  left_join(county.fips, by = "polyname") %>%
+  rename(county_fips = fips) %>%
 
   # relevant columns
 
