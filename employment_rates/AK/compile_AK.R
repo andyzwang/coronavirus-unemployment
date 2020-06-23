6056a19b091fa70fc31c82c6ef76a21c5002eddc
Andy Wang <azwang@college.harvard.edu>
2020-06-23 16:14:00 -0500

    compile alaska

---
+++ employment_rates/AK/compile_AK.R
@@ -0,0 +1,50 @@
+# imports
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(maps)
+library(stringr)
+
+# import
+
+raw <- read_csv("AK_data.csv", skip = 2) %>%
+  clean_names(case = "snake") 
+
+output <- raw %>%
+  
+  filter(year >= 2019) %>%
+  filter(area_type != 60) %>%
+  filter(period != 0) %>%
+  mutate(
+    # set basics
+    
+    area = area_name,
+    area_type = case_when(
+      area_type == 1 ~ "State",
+      area_type == 4 ~ "County",
+      area_type == 2 ~ "City",
+    ),
+    
+    state_fips = "03",
+    state_short = "AK",
+    state = "Alaska",
+    
+    # work on FIPS
+    fips = case_when(
+      area_type == "County" ~ 
+        paste("03", str_pad(area_code, 3, pad = "0"), sep = "")
+    ),
+    
+    # dates
+    date = ceiling_date(mdy(paste(period, "1", year, sep = "/")), "months") - 1,
+    month = month(date),
+    year = year(date),
+    week = NA
+  ) %>%
+  select(
+    state_fips, state_short, state, area, area_type, fips, date,
+    employment, labor_force, unemployment
+  )
+
+write.csv(output, file="AK_compiled.csv", row.names = FALSE)
