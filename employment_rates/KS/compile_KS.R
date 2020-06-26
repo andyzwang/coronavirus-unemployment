98eab2c881554387d5cbc8adcea0f701c9ec383b
Andy Wang <azwang@college.harvard.edu>
2020-06-26 10:52:14 -0500

    compile KS

---
+++ employment_rates/KS/compile_KS.R
@@ -0,0 +1,39 @@
+# imports
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(maps)
+library(stringr)
+
+# import
+
+raw <- read_csv("KS_data.csv") %>%
+  clean_names("snake")
+
+
+output <- raw %>%
+  mutate(
+    state_fips = "20",
+    state_short = "KS",
+    state = "Kansas",
+    area = str_to_title(area),
+    year = 2020,
+    polyname = case_when(
+      area_type == "county" ~ paste("kansas,", tolower(str_remove(str_remove(area, "[[:punct:]]"), " County")), sep = "")
+    )
+  ) %>%
+
+  # Join with FIPS
+
+  left_join(county.fips, by = "polyname") %>%
+  rename(
+    employment = employed,
+    unemployment = unemployed,
+  ) %>%
+  select(
+    state_fips, state_short, state, area, area_type, fips, period, year,
+    employment, labor_force, unemployment
+  )
+
+write.csv(output, file = "KS_compiled.csv", row.names = FALSE)
