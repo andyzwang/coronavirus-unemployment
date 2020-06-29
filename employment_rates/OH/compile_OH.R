6aaed9e50b1465215d57add34098240015b265a7
Andy Wang <azwang@college.harvard.edu>
2020-06-29 09:55:25 -0500

    compile OH

---
+++ employment_rates/OH/compile_OH.R
@@ -0,0 +1,45 @@
+# imports
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(maps)
+library(stringr)
+# import
+
+raw <- read_csv("OH_data.csv")
+
+
+output <- raw %>%
+  clean_names("snake") %>%
+  mutate(
+    state_fips = "39",
+    state_short = "OH",
+    state = "Ohio",
+    area = str_to_title(area),
+    area_type = str_to_lower(area_type),
+    period = month(mdy(paste(period, "-01-2020", sep = "")), label = T, abbr = F),
+    polyname = case_when(
+      area_type == "county" ~ paste("ohio,",
+        tolower(str_remove(str_remove(
+          area,
+          "[[:punct:]]"
+        ), " County")),
+        sep = ""
+      )
+    )
+  ) %>%
+
+  # Join with FIPS
+
+  left_join(county.fips, by = "polyname") %>%
+  rename(
+    employment = employed,
+    unemployment = unemployed
+  ) %>%
+  select(
+    state_fips, state_short, state, area, area_type, fips, period, year,
+    employment, labor_force, unemployment
+  ) 
+
+write.csv(output, file = "OH_compiled.csv", row.names = FALSE)
---
+++ employment_rates/OR/compile_OR.R
@@ -0,0 +1,55 @@
+# imports
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(maps)
+library(stringr)
+# import
+
+em <- read_csv("OR_em_data.csv", na= c("", NA, "-")) %>%
+  mutate(mark = "employment")
+unem <- read_csv("OR_unem_data.csv", na= c("", NA, "-"), skip = 4)%>%
+  mutate(mark = "unemployment")
+lf <- read_csv("OR_lf_data.csv", na= c("", NA, "-")) %>%
+  mutate(mark = "labor_force")
+
+raw <- rbind(em, unem, lf)
+
+output <- raw %>%
+  remove_empty("cols") %>%
+  pivot_longer(cols = contains("-"), names_to = "date", values_to = "value") %>%
+  clean_names("snake") %>%
+  pivot_wider(names_from = "mark", values_from = "value") %>%
+  mutate(
+    state_fips = "41",
+    state_short = "OR",
+    state = "Oregon",
+    area = str_remove(area," \\(Not Seasonally Adjusted\\)"),
+    area_type = case_when(
+      str_detect(area, "County") ~ "county",
+      area == "Oregon" ~ "state"
+    ),
+    period = month(ymd(paste("20", date, "-01", sep = "")), label = T, abbr = F),
+    year = year(ymd(paste(date, "-01", sep = ""))),
+    polyname = case_when(
+      area_type == "county" ~ paste("oregon,",
+        tolower(str_remove(str_remove(
+          area,
+          "[[:punct:]]"
+        ), " County")),
+        sep = ""
+      )
+    )
+  ) %>%
+
+  # Join with FIPS
+
+  left_join(county.fips, by = "polyname") %>%
+  select(
+    state_fips, state_short, state, area, area_type, fips, period, year,
+    employment, labor_force, unemployment
+  )  %>%
+  filter(!is.na(area_type))
+
+write.csv(output, file = "OR_compiled.csv", row.names = FALSE)
