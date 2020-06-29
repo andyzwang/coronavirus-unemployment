447b0accfc44b2991a7ea4437841c08728014a7b
Andy Wang <azwang@college.harvard.edu>
2020-06-29 12:41:54 -0500

    add WY and WI

---
+++ employment_rates/WI/compile_WI.R
@@ -0,0 +1,50 @@
+# imports
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(maps)
+library(stringr)
+
+# import
+
+raw <- read_csv("WI_data.csv")
+
+output <- raw %>%
+  clean_names("snake") %>%
+  mutate(
+    state_fips = "55",
+    state_short = "WI",
+    state = "Wisconsin",
+    area_type = case_when(
+      area == "Wisconsin" ~ "state",
+      str_detect(area, "County") ~ "county",
+      TRUE ~ "city"
+      ),
+    polyname = case_when(
+      area_type == "county" ~ paste("wisconsin,",
+                                    tolower(str_remove(str_remove(
+                                      area,
+                                      "[[:punct:]]"
+                                    ), " County")),
+                                    sep = ""
+      )
+    )
+  ) %>%
+  
+  # Join with FIPS
+  
+  left_join(county.fips, by = "polyname") %>%
+  filter(!str_detect(area, "-.*County")) %>%
+  mutate(
+    area = case_when(
+      str_detect(area, "-.*Counties") ~ str_remove(area, "-.*Counties"),
+      TRUE ~ area
+    )
+  ) %>%
+   select(
+    state_fips, state_short, state, area, area_type, fips, period, year,
+    employment, labor_force, unemployment
+  )
+
+write.csv(output, file = "WI_compiled.csv", row.names = FALSE)
---
+++ employment_rates/WY/compile_WY.R
@@ -0,0 +1,40 @@
+# imports
+
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(maps)
+library(stringr)
+
+# import
+
+raw <- read_csv("WY_compiled_data.csv")
+
+output <- raw %>%
+  clean_names("snake") %>%
+  mutate(
+    state_fips = "56",
+    state_short = "WY",
+    state = "Wyoming",
+    year = 2020,
+    area_type = str_to_lower(area_type),
+    polyname = case_when(
+      area_type == "county" ~ paste("wyoming,",
+                                    tolower(str_remove(str_remove(
+                                      area,
+                                      "[[:punct:]]"
+                                    ), " County")),
+                                    sep = ""
+      )
+    )
+  ) %>%
+  
+  # Join with FIPS
+  
+  left_join(county.fips, by = "polyname")  %>%
+   select(
+    state_fips, state_short, state, area, area_type, fips, period, year,
+    employment, labor_force, unemployment
+  )
+
+write.csv(output, file = "WY_compiled.csv", row.names = FALSE)
